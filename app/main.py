663aa05a7623c2683c3d98085f6e8a3b15a14d31
JanJorquera <janjorquera@gmail.com>
2025-06-06 17:02:13 -0400

    BaseModel test

--- app/main.py
+++ app/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class User(BaseModel):
+	name: str
+	surname: str
+	url: str
+	age: int
 
 @app.get("/")
 async def root():
 	return {"message": "Hello World"}
+
+#Documentacion https://127.0.0.1/docs
+#Documentacion https://127.0.0.1/redoc
+
+#Levantar server: uvicorn main:app --reload
