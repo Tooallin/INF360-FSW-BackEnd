4492310b0793e99747647ba26e72234c25dbb082
Tooallin <cintolesia@gmail.com>
2025-06-06 22:36:06 -0400

    [Change] CRUD Users Funcional

--- app/db/models/user.py
+++ app/db/models/user.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, String
 from app.db.base import Base
 
 class User(Base):
-	__tablename__ = "Users"
+	__tablename__ = "users"
 
 	id = Column(Integer, primary_key=True, index=True)
 	name = Column(String, nullable=False)
