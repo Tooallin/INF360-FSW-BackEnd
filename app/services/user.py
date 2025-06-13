30a4dc7c94c1429217fdbb3a83264472a73000a3
JanJorquera <janjorquera@gmail.com>
2025-06-13 15:47:50 -0400

    [Add] get_id del token

--- app/services/user.py
+++ app/services/user.py
@@ -40,6 +40,22 @@ def auth_user_by_credentials(user: UserLogin, db: Session):
 	
 	return user_get.id
 
+def get_id(jwt_token: str):
+	exception = HTTPException(
+		status_code=status.HTTP_401_UNAUTHORIZED,
+		detail="Credenciales de autenticación inválidas",
+		headers={"WWW-Authenticate": "Bearer"})
+	
+	try:
+		username = jwt.decode(jwt_token, os.getenv("SECRET"), algorithms=[os.getenv("ALGORITHM")]).get("sub")
+		if username is None:
+			raise exception
+
+	except JWTError:
+		raise exception
+
+	return username
+
 '''
 #Función pendiente por implementar
 async def auth_user_by_jwt(user: UserLogin, db: Session):
