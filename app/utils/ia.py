cde4122664cdfb83d9a94387d8469c8eb010acb3
JanJorquera <janjorquera@gmail.com>
2025-06-13 17:29:58 -0400

    [Change] timeout

--- app/utils/ia.py
+++ app/utils/ia.py
@@ -15,7 +15,7 @@ def generate(message: str):
 		},
 	}
 
-	response = requests.post(url=url, headers=headers, data=json.dumps(data))
+	response = requests.post(url=url, headers=headers, data=json.dumps(data), timeout=(5, 30))
 	response_text = json.loads(response.text)['response']
 	ia_response = re.sub(r'<think>.*?</think>', '', response_text, flags=re.DOTALL)
 	return ia_response.strip()
