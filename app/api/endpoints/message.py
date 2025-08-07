8b5f75e5c3ba9a86238e4a9ea11ffb97b962f9f0
JanJorquera <janjorquera@gmail.com>
2025-08-06 22:37:43 -0400

    [Update] Avance en autenticación para el uso de mensajes

--- app/api/endpoints/message.py
+++ app/api/endpoints/message.py
@@ -4,17 +4,18 @@ from app.schemas.message import MessageCreate, MessageOut
 from app.services.message import create, get_all, create_base
 from typing import List
 from sqlalchemy.orm import Session
+from app.utils.jwt_decode import get_id
 
 router = APIRouter()
 
 @router.post("/create", response_model=MessageOut)
-def Create(message: MessageCreate, db: Session = Depends(deps.get_db)):
-	return create(message=message, db=db)
+def Create(message: MessageCreate, db: Session = Depends(deps.get_db), id_user: int = Depends(get_id)):
+	return create(message=message, db=db, id_user=id_user)
 
 @router.get("/createbase", response_model=MessageOut)
-def CreateBase():
+def CreateBase(id_user: int = Depends(get_id)):
 	return create_base()
 
 @router.get("/getall/{id_chat}", response_model=List[MessageOut])
-def GetAll(id_chat: int, db: Session = Depends(deps.get_db)):
-	return get_all(id_chat=id_chat, db=db)
+def GetAll(id_chat: int, db: Session = Depends(deps.get_db), id_user: int = Depends(get_id)):
+	return get_all(id_chat=id_chat, db=db, id_user=id_user)
--- app/services/message.py
+++ app/services/message.py
@@ -1,6 +1,7 @@
 from app.utils import ia
 from app.schemas.message import MessageCreate, MessagePack, MessageOut
 from app.crud import message as Crud
+from app.crud import chat as CrudChat
 from sqlalchemy.orm import Session
 from app.utils import translate
 from fastapi import HTTPException, status
@@ -25,7 +26,18 @@ def create_base():
 
 	return response
 
-def create(message: MessageCreate, db: Session):
+def create(message: MessageCreate, db: Session, id_user: int):
+	# Alguien intentando añadir un mensaje de un chat que no es suyo
+	chats = CrudChat.get_all(db, id_user)
+
+	if chats.filter(chats.id_chat == message.id_chat) is None:
+		raise HTTPException(
+			status_code=status.HTTP_404_NOT_FOUND,
+			detail="Ocurrió un error inesperado"
+		)
+
+	# Si intenta añadir a mensajes a un chat suyo se continua con el comportamiento esperado
+
 	#DE AQUI PARA ABAJO DEBERIA SER UN TRY-CATCH
 
 	exception = HTTPException(
@@ -44,6 +56,7 @@ def create(message: MessageCreate, db: Session):
 		)
 
 
+	'''
 	response = MessageOut(
 		id=1,
 		id_chat=-1,
@@ -51,17 +64,28 @@ def create(message: MessageCreate, db: Session):
 		#ai_response=ai_response
 		ai_response=ai_response_spanish
 	)
+	'''
 
 	# AQUI UN CASO DE ERROR EN LA BD
 	# ADEMÁS HAY QUE MODIFICAR LO QUE ESPERA Crud.create()}
 	
-	#response = Crud.create(db, MessagePack(
-	#	id_chat=message.id_chat,
-	#	user_question=message.user_question,
-	#	ai_response=ia_response_spanish
-	#))
+	response = Crud.create(db, MessagePack(
+		id_chat=message.id_chat,
+		user_question=message.user_question,
+		ai_response=ia_response_spanish
+	))
 
 	return response
 
-def get_all(id_chat: int, db: Session):
+def get_all(id_chat: int, db: Session, id_user: int):
+	# Alguien intentando obtener mensajes de un chat que no es suyo
+	chats = CrudChat.get_all(db, id_user)
+
+	if chats.filter(chats.id_chat == id_chat) is None:
+		raise HTTPException(
+			status_code=status.HTTP_404_NOT_FOUND,
+			detail="Ocurrió un error inesperado"
+		)
+
+	# Si intenta acceder a mensajes de un chat suyo se retorna lo correspondiente
 	return Crud.get_all(db, id_chat)
